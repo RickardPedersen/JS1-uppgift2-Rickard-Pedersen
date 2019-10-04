6d04482a9f887861c2a20832fa2ea68e4d22ec57
RickardPedersen <54976141+RickardPedersen@users.noreply.github.com>
2019-10-04 12:22:26 +0200

    Nu funkar json-filen. Började bygga correct funktion.

--- Quiz/main.js
+++ Quiz/main.js
@@ -1,3 +1,8 @@
+// TODO: alreadyAnswerd. kanske i json?
+
+window.addEventListener('DOMContentLoaded', (event) => {
+
+
 class Quiz {
     constructor(playerName, questions) {
         this.playerName = playerName;
@@ -15,12 +20,50 @@ class Question {
     }
 }
 
-function selectAnswer() {
-    alert("Checked");
+function selectAnswer(event) {
+    console.log(event.currentTarget.parentNode);
+    if (event.currentTarget.checked == true) {
+        event.currentTarget.parentNode.classList.add("selected");
+    } else {
+        event.currentTarget.parentNode.classList.remove("selected");
+    }
 }
 
-document.getElementById("cb1").addEventListener("click", selectAnswer);
-document.getElementById("cb2").addEventListener("click", selectAnswer);
-document.getElementById("cb3").addEventListener("click", selectAnswer);
-document.getElementById("cb4").addEventListener("click", selectAnswer);
+document.getElementById("cb1").addEventListener("change", selectAnswer);
+document.getElementById("cb2").addEventListener("change", selectAnswer);
+document.getElementById("cb3").addEventListener("change", selectAnswer);
+document.getElementById("cb4").addEventListener("change", selectAnswer);
+
+
+let json = getJSON('my_data.json');
+console.log(json);
+
+document.getElementById("question").innerHTML = json.Gaming[0].fråga;
+
+for (let i = 0; i < json.Gaming.length ; i++) {
+    document.getElementById("answer"+i).innerHTML = json.Gaming[0].answers[i].alternativ;
+}
+
+document.getElementById("chooseButton").addEventListener("click", function (event) {
+    
+    for (let i = 0; i < document.getElementsByClassName("cb").length; i++) {
+        if (document.getElementsByClassName("cb")[i].checked === json.Gaming[0].answers[i].correct) {
+            if (i == 3) {
+                alert("rätt");
+                break;
+            }
+        } else {
+            alert("fel");
+            break;
+        }
+    }
+    
+   
+    //console.log(document.getElementsByClassName("cb")[0].checked)
+    //console.log(json.Gaming[0].answers[0].correct)
+})
+
+
+
+});
 
